6a4276fbd6730a40b885a3d575376a27bf283988
Manjot Singh <129639545+ManjotSingh2k05@users.noreply.github.com>
2025-11-18 12:35:56 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+        int sum = 0;
+        for(int i=0;i<n;i++) {
+            sum += nums[i];
+            int rem = sum - k;
+            if(mp.find(rem) != mp.end()){
+                count += mp[rem];
+            } 
+            mp[sum] += 1;
+        }
+        return count;
+    }
+};
